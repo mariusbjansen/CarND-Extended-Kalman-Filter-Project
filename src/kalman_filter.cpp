ee2a2dcdba6c55fc652de44b41c8cd777ac5e91e
Marius Jansen <mariusb.jansen@googlemail.com>
2018-01-06 19:02:20 +0100

    started Kalman Filter Predict and Update functions

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -21,10 +21,10 @@ void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
 }
 
 void KalmanFilter::Predict() {
-  /**
-  TODO:
-    * predict the state
-  */
+
+  x_ = F_*x_;
+  MatrixXd Ft = F_.transpose();
+  P_ = F_ * P_ * Ft + Q_;
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
@@ -32,6 +32,18 @@ void KalmanFilter::Update(const VectorXd &z) {
   TODO:
     * update the state by using Kalman Filter equations
   */
+
+  VectorXd y = z - H_ * x_;
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd K = P_ * Ht * Si;
+
+  // new state
+  x_ = x_ + (K * y);
+  MatrixXd I = MatrixXd::Identity(2, 2);
+  P_ = (I - K * H_) * P_;
+  
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
@@ -39,4 +51,20 @@ void KalmanFilter::UpdateEKF(const VectorXd &z) {
   TODO:
     * update the state by using Extended Kalman Filter equations
   */
+
+  // convert state x' to polar = radar COordinate System (cos)
+  VectorXd x_radar_cos = VectorXd(3);
+
+  x_radar_cos(0) = sqrt(x_(0)*x_(0)+x_(1)*x_(1));
+  x_radar_cos(1) = atan2(x_(1),x_(0));
+  x_radar_cos(2) = (x_(0)*x_(2)+x_(1)*x_(3))/sqrt(x_radar_cos(0));
+
+  VectorXd y = z-x_radar_cos;
+  
+  /**
+  TODO:
+    * continue here with S, K and P next time
+    * update the state by using Extended Kalman Filter equations
+  */
+
 }
